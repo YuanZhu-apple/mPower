d621cf650fbdd9e42cfe2efddd0fd7cbc33d1c3e
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-08-25 15:07:04 -0700

    Updated Submodule & Instantiated DataSubstrate

--- Parkinson/AppDelegate.m
+++ Parkinson/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+static NSString * databaseName = @"db.sqlite";
+
 @interface AppDelegate ()
 
 @end
@@ -18,7 +20,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.networkManager = [[APCSageNetworkManager alloc] initWithBaseURL:@"http://localhost:4567/api/v1"];
+    self.dataSubstrate = [[APCDataSubstrate alloc] initWithPersistentStorePath:[[self applicationDocumentsDirectory] stringByAppendingPathComponent:databaseName] additionalModels: nil];
     return YES;
+}
+
+- (NSString *) applicationDocumentsDirectory
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *basePath = ([paths count] > 0) ? paths[0] : nil;
+    return basePath;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
