737e55a271fa60e271389d0794920af3d6666c33
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-08-28 08:38:16 -0700

    Updated Submodules Added Entitlements

--- Parkinson/APHParkinsonAppDelegate.m
+++ Parkinson/APHParkinsonAppDelegate.m
@@ -53,4 +53,9 @@ NSString *const ParkinsonStudyIdentifier = @"com.ymedialabs.parkinsons";
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void)application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
+{
+    
+}
+
 @end
