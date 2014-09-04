4bd343950bffb3fdba4c774fde4bc219aac000e6
Justin Warmkessel <justin.w@ymedialabs.com>
2014-09-04 16:10:04 -0700

    Displaying study demos tapping task.

--- Parkinson/IntervalTappingControllers/APHIntervalTappingTaskViewController.m
+++ Parkinson/IntervalTappingControllers/APHIntervalTappingTaskViewController.m
@@ -26,7 +26,38 @@
 
 + (RKTask *)createTask
 {
-    return  nil;
+    
+    NSMutableArray *steps = [[NSMutableArray alloc] init];
+    {
+        RKActiveStep* step = [[RKActiveStep alloc] initWithIdentifier:@"aid_001a" name:@"active step"];
+        step.caption = @"Touch";
+        step.text = @"An active test, touch collection";
+        step.clickButtonToStartTimer = YES;
+        step.countDown = 30.0;
+        step.voicePrompt = @"An active test, touch collection";
+        step.recorderConfigurations = @[[RKTouchRecorderConfiguration configuration]];
+        [steps addObject:step];
+    }
+    
+    {
+        RKActiveStep* step = [[RKActiveStep alloc] initWithIdentifier:@"aid_001b" name:@"active step"];
+        step.caption = @"Button Tap";
+        step.text = @"Please tap the orange button above when it appears.";
+        step.countDown = 10.0;
+        //step.recorderConfigurations = @[[CustomRecorderConfiguration new]];
+        [steps addObject:step];
+    }
+    
+    {
+        RKActiveStep* step = [[RKActiveStep alloc] initWithIdentifier:@"aid_001c" name:@"active step"];
+        step.caption = @"Motion";
+        step.text = @"An active test collecting device motion data";
+        step.recorderConfigurations = @[ [[RKDeviceMotionRecorderConfiguration alloc] initWithFrequency:100.0]];
+        [steps addObject:step];
+    }
+    
+    RKTask  *task = [[RKTask alloc] initWithName:@"Interval Touches" identifier:@"Tapping Task" steps:steps];
+    return  task;
 }
 
 @end
