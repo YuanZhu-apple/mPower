6c740534b19147c9a8d40893de5a75c07838a940
Henry McGilton <henry.mcgilton@ymedialabs.com>
2014-09-08 14:16:31 -0700

    Changed Incorrect Copyright Notice

--- Parkinson/ApplicationControllers/APHLearnDetailViewController.m
+++ Parkinson/ApplicationControllers/APHLearnDetailViewController.m
@@ -3,7 +3,7 @@
 //  BasicTabBar
 //
 //  Created by Henry McGilton on 9/7/14.
-//  Copyright (c) 2014 Trilithon Software. All rights reserved.
+//  Copyright (c) 2014 2014 Y Media Labs. All rights reserved.
 //
 
 #import "APHLearnDetailViewController.h"
