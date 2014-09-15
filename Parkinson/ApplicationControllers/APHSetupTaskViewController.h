48f0651349dd109ca301234847d812230a588a93
Dhanush Balachandran <dhanush.balachandran@ymedialabs.com>
2014-09-15 10:35:55 -0700

    Fixing Merge Issue

--- Parkinson/ApplicationControllers/APHSetupTaskViewController.h
+++ Parkinson/ApplicationControllers/APHSetupTaskViewController.h
@@ -14,6 +14,6 @@
 
 @property  (nonatomic, strong)  APCScheduledTask  *scheduledTask;
 
-+ (instancetype)customTaskViewController;
++ (instancetype)customTaskViewController: (APCScheduledTask*) scheduledTask;
 
 @end
