4007805c7ef83e450d58c6e86ec1246ff9bbced2
Justin Warmkessel <justin.w@ymedialabs.com>
2014-10-15 16:05:56 -0700

    Added delegates to walking task, phonation task, and survey collection. Collecting data using RKDataArchive and persisting.

--- Parkinson/TasksAndSteps/PhonationControllers/APHPhonationTaskViewController.m
+++ Parkinson/TasksAndSteps/PhonationControllers/APHPhonationTaskViewController.m
@@ -11,11 +11,19 @@
 #import <objc/message.h>
 #import <AVFoundation/AVFoundation.h>
 
+static NSString *MainStudyIdentifier = @"com.parkinsons.phonation";
+
 static NSString * kPhonationStep101Key = @"Phonation_Step_101";
 static NSString * kPhonationStep102Key = @"Phonation_Step_102";
 static NSString * kPhonationStep103Key = @"Phonation_Step_103";
 static NSString * kPhonationStep104Key = @"Phonation_Step_104";
 static NSString * kPhonationStep105Key = @"Phonation_Step_105";
+
+@interface APHPhonationTaskViewController ()
+
+@property (strong, nonatomic) RKDataArchive *taskArchive;
+
+@end
 
 @implementation APHPhonationTaskViewController
 
@@ -48,7 +56,6 @@ static NSString * kPhonationStep105Key = @"Phonation_Step_105";
     {
         RKActiveStep* step = [[RKActiveStep alloc] initWithIdentifier:kPhonationStep103Key name:@"active step"];
         step.caption = @"Great Job!";
-        step.countDown = 5.0;
         [steps addObject:step];
     }
     
@@ -85,5 +92,114 @@ static NSString * kPhonationStep105Key = @"Phonation_Step_105";
     controller.step = step;
     return  controller;
 }
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self beginTask];
+}
+
+/*********************************************************************************/
+#pragma  mark  - Private methods
+/*********************************************************************************/
+
+- (void)beginTask
+{
+    if (self.taskArchive)
+    {
+        [self.taskArchive resetContent];
+    }
+    
+    self.taskArchive = [[RKDataArchive alloc] initWithItemIdentifier:[RKItemIdentifier itemIdentifierForTask:self.task] studyIdentifier:MainStudyIdentifier taskInstanceUUID:self.taskInstanceUUID extraMetadata:nil fileProtection:RKFileProtectionCompleteUnlessOpen];
+    
+}
+
+/*********************************************************************************/
+#pragma mark - Helpers
+/*********************************************************************************/
+
+-(void)sendResult:(RKResult*)result
+{
+    // In a real application, consider adding to the archive on a concurrent queue.
+    NSError *err = nil;
+    if (![result addToArchive:self.taskArchive error:&err])
+    {
+        // Error adding the result to the archive; archive may be invalid. Tell
+        // the user there's been a problem and stop the task.
+        NSLog(@"Error adding %@ to archive: %@", result, err);
+    }
+}
+
+
+/*********************************************************************************/
+#pragma  mark  - TaskViewController delegates
+/*********************************************************************************/
+- (void)taskViewController:(RKTaskViewController *)taskViewController didProduceResult:(RKResult *)result {
+    
+    NSLog(@"didProduceResult = %@", result);
+    
+    if ([result isKindOfClass:[RKSurveyResult class]]) {
+        RKSurveyResult* sresult = (RKSurveyResult*)result;
+        
+        for (RKQuestionResult* qr in sresult.surveyResults) {
+            NSLog(@"%@ = [%@] %@ ", [[qr itemIdentifier] stringValue], [qr.answer class], qr.answer);
+        }
+    }
+    
+    
+    [self sendResult:result];
+    
+    [super taskViewController:taskViewController didProduceResult:result];
+}
+
+- (void)taskViewControllerDidFail: (RKTaskViewController *)taskViewController withError:(NSError*)error{
+    
+    [self.taskArchive resetContent];
+    self.taskArchive = nil;
+    
+}
+
+- (void)taskViewControllerDidCancel:(RKTaskViewController *)taskViewController{
+    
+    [taskViewController suspend];
+    
+    [self.taskArchive resetContent];
+    self.taskArchive = nil;
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)taskViewControllerDidComplete: (RKTaskViewController *)taskViewController{
+    
+    [taskViewController suspend];
+    
+    NSError *err = nil;
+    NSURL *archiveFileURL = [self.taskArchive archiveURLWithError:&err];
+    if (archiveFileURL)
+    {
+        NSURL *documents = [NSURL fileURLWithPath:[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject]];
+        NSURL *outputUrl = [documents URLByAppendingPathComponent:[archiveFileURL lastPathComponent]];
+        
+        // This is where you would queue the archive for upload. In this demo, we move it
+        // to the documents directory, where you could copy it off using iTunes, for instance.
+        [[NSFileManager defaultManager] moveItemAtURL:archiveFileURL toURL:outputUrl error:nil];
+        
+        NSLog(@"outputUrl= %@", outputUrl);
+        
+        // When done, clean up:
+        self.taskArchive = nil;
+        if (archiveFileURL)
+        {
+            [[NSFileManager defaultManager] removeItemAtURL:archiveFileURL error:nil];
+        }
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    [super taskViewControllerDidComplete:taskViewController];
+}
+
+
 
 @end
--- Parkinson/TasksAndSteps/WalkingControllers/APHWalkingTaskViewController.m
+++ Parkinson/TasksAndSteps/WalkingControllers/APHWalkingTaskViewController.m
@@ -13,6 +13,8 @@
 #import "APHWalkingResultsViewController.h"
 #import <objc/message.h>
 
+static NSString *MainStudyIdentifier = @"com.parkinsons.walkingTask";
+
 static  NSString  *kWalkingStep101Key = @"Walking Step 101";
 static  NSString  *kWalkingStep102Key = @"Walking Step 102";
 static  NSString  *kWalkingStep103Key = @"Walking Step 103";
@@ -23,6 +25,8 @@ static  NSString  *kWalkingStep105Key = @"Walking Step 105";
 {
     NSInteger _count;
 }
+
+@property (strong, nonatomic) RKDataArchive *taskArchive;
 
 @end
 
@@ -144,6 +148,123 @@ static  NSString  *kWalkingStep105Key = @"Walking Step 105";
         controller.delegate = self;
     }
     return  controller;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self beginTask];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+/*********************************************************************************/
+#pragma  mark  - Private methods
+/*********************************************************************************/
+
+- (void)beginTask
+{
+    if (self.taskArchive)
+    {
+        [self.taskArchive resetContent];
+    }
+    
+    self.taskArchive = [[RKDataArchive alloc] initWithItemIdentifier:[RKItemIdentifier itemIdentifierForTask:self.task] studyIdentifier:MainStudyIdentifier taskInstanceUUID:self.taskInstanceUUID extraMetadata:nil fileProtection:RKFileProtectionCompleteUnlessOpen];
+    
+}
+
+/*********************************************************************************/
+#pragma mark - Helpers
+/*********************************************************************************/
+
+-(void)sendResult:(RKResult*)result
+{
+    // In a real application, consider adding to the archive on a concurrent queue.
+    NSError *err = nil;
+    if (![result addToArchive:self.taskArchive error:&err])
+    {
+        // Error adding the result to the archive; archive may be invalid. Tell
+        // the user there's been a problem and stop the task.
+        NSLog(@"Error adding %@ to archive: %@", result, err);
+    }
+}
+
+
+/*********************************************************************************/
+#pragma  mark  - TaskViewController delegates
+/*********************************************************************************/
+- (void)taskViewController:(RKTaskViewController *)taskViewController didProduceResult:(RKResult *)result {
+    
+    NSLog(@"didProduceResult = %@", result);
+    
+    if ([result isKindOfClass:[RKSurveyResult class]]) {
+        RKSurveyResult* sresult = (RKSurveyResult*)result;
+        
+        for (RKQuestionResult* qr in sresult.surveyResults) {
+            NSLog(@"%@ = [%@] %@ ", [[qr itemIdentifier] stringValue], [qr.answer class], qr.answer);
+        }
+    }
+    
+    
+    [self sendResult:result];
+    
+    [super taskViewController:taskViewController didProduceResult:result];
+}
+
+- (void)taskViewControllerDidFail: (RKTaskViewController *)taskViewController withError:(NSError*)error{
+    
+    [self.taskArchive resetContent];
+    self.taskArchive = nil;
+    
+}
+
+- (void)taskViewControllerDidCancel:(RKTaskViewController *)taskViewController{
+    
+    [taskViewController suspend];
+    
+    [self.taskArchive resetContent];
+    self.taskArchive = nil;
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)taskViewControllerDidComplete: (RKTaskViewController *)taskViewController{
+    
+    [taskViewController suspend];
+    
+    NSError *err = nil;
+    NSURL *archiveFileURL = [self.taskArchive archiveURLWithError:&err];
+    if (archiveFileURL)
+    {
+        NSURL *documents = [NSURL fileURLWithPath:[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject]];
+        NSURL *outputUrl = [documents URLByAppendingPathComponent:[archiveFileURL lastPathComponent]];
+        
+        // This is where you would queue the archive for upload. In this demo, we move it
+        // to the documents directory, where you could copy it off using iTunes, for instance.
+        [[NSFileManager defaultManager] moveItemAtURL:archiveFileURL toURL:outputUrl error:nil];
+        
+        NSLog(@"outputUrl= %@", outputUrl);
+        
+        // When done, clean up:
+        self.taskArchive = nil;
+        if (archiveFileURL)
+        {
+            [[NSFileManager defaultManager] removeItemAtURL:archiveFileURL error:nil];
+        }
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    [super taskViewControllerDidComplete:taskViewController];
 }
 
 
