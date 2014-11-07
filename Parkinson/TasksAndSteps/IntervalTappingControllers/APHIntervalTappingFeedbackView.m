3809c6f396b41fa456a2fe1f0163496415deba4d
Henry McGilton <henry.mcgilton@ymedialabs.com>
2014-11-07 09:29:26 -0800

    Changed Gradient Creation Mechanism

--- Parkinson/TasksAndSteps/IntervalTappingControllers/APHIntervalTappingFeedbackView.m
+++ Parkinson/TasksAndSteps/IntervalTappingControllers/APHIntervalTappingFeedbackView.m
@@ -8,29 +8,27 @@
 
 #import "APHIntervalTappingFeedbackView.h"
 
-static  CGGradientRef  target = NULL;
-
 @interface APHIntervalTappingFeedbackView  ( )
+
+@property  (nonatomic, assign)  CGGradientRef  gradient;
 
 @end
 
 @implementation APHIntervalTappingFeedbackView
 
-+ (void)initialize
+- (void)createGradient
 {
-    if (target == NULL) {
-        CGColorSpaceRef  colorSpace = CGColorSpaceCreateDeviceRGB();
-        CGFloat  lite[4] = { 0.9375, 0.9375, 1.0, 0.75 };    //    light color
-        CGFloat  dark[4] = { 0.5000, 0.5000, 1.0, 1.00 };    //    dark color
-        
-        CGFloat  locations[] = { 1.0, 0.0 };
-        CGColorRef  startColorRef  = CGColorCreate(colorSpace, lite);
-        CGColorRef  endColorRef    = CGColorCreate(colorSpace, dark);
-        
-        NSArray  *colors = @[(__bridge NSObject *)startColorRef, (__bridge NSObject *)endColorRef];
-        CGGradientRef  aGradient = CGGradientCreateWithColors(colorSpace, (__bridge CFArrayRef)colors, locations);
-        target = aGradient;
-    }
+    CGColorSpaceRef  colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGFloat  lite[4] = { 0.9375, 0.9375, 1.0, 0.75 };    //    light color
+    CGFloat  dark[4] = { 0.5000, 0.5000, 1.0, 1.00 };    //    dark color
+    
+    CGFloat  locations[] = { 1.0, 0.0 };
+    CGColorRef  startColorRef  = CGColorCreate(colorSpace, lite);
+    CGColorRef  endColorRef    = CGColorCreate(colorSpace, dark);
+    
+    NSArray  *colors = @[(__bridge NSObject *)startColorRef, (__bridge NSObject *)endColorRef];
+    CGGradientRef  aGradient = CGGradientCreateWithColors(colorSpace, (__bridge CFArrayRef)colors, locations);
+    self.gradient = aGradient;
 }
 
 - (BOOL)isOpaque
@@ -40,16 +38,18 @@ static  CGGradientRef  target = NULL;
 
 - (void)drawRect:(CGRect)rect
 {
+    [self createGradient];
+    
     CGContextRef  context = UIGraphicsGetCurrentContext();
     CGRect  bounds = self.bounds;
-    CGContextDrawRadialGradient(context, target, CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds)), 0.0,
+    CGContextDrawRadialGradient(context, self.gradient, CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds)), 0.0,
                                 CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds)), CGRectGetWidth(bounds) / 2.0, 0);
 }
 
 - (void)dealloc
 {
-    if (target != NULL) {
-        CGGradientRelease(target);
+    if (_gradient != NULL) {
+        CGGradientRelease(_gradient);
     }
 }
 
