16abec3c6adf174044eb21ca69e52ba95798358d
Henry McGilton <henry.mcgilton@ymedialabs.com>
2014-11-30 18:32:26 -0800

    Adding Decoration to Phonation Feedback

--- Parkinson/TasksAndSteps/PhonationControllers/APHPhonationMeteringView.m
+++ Parkinson/TasksAndSteps/PhonationControllers/APHPhonationMeteringView.m
@@ -47,7 +47,7 @@ static  CGFloat  kDisabledPowerLevel = -1.0;
 {
     self.disabledStrokeColor = [UIColor grayColor];
     
-    self.innerDiskColor       = [UIColor lightGrayColor];
+    self.innerDiskColor       = [UIColor darkGrayColor];
     self.innerRingFillColor   = [UIColor whiteColor];
     self.middleRingColor      = [UIColor greenColor];
     self.outerRingColor       = [UIColor redColor];
@@ -75,6 +75,65 @@ static  CGFloat  kDisabledPowerLevel = -1.0;
     self.outerRingRectangle  = self.bounds;
     
     self.powerLevel = kDisabledPowerLevel;
+}
+
+    //
+    //    draws four periods of a sine wave (actually some kind of Conic Section)
+    //
+- (void)drawInnerDiskInContext:(CGContextRef)context withBounds:(CGRect)bounds
+{
+    CGContextAddEllipseInRect(context, self.innerDiskRectangle);
+    CGContextSetFillColorWithColor(context, self.innerDiskColor.CGColor);
+    CGContextFillPath(context);
+    
+    CGContextSaveGState(context);
+    
+        //
+        //    fine tuning magic numbers for translation and scale
+        //
+    CGContextTranslateCTM(context, CGRectGetMinX(self.innerDiskRectangle) + 3.0 * CGRectGetWidth(bounds) / 64.0,
+                          CGRectGetMinY(self.innerDiskRectangle) + 1.5 * CGRectGetWidth(bounds) / 64.0);
+    CGContextScaleCTM(context, 0.04375, 0.20);
+    
+    CGContextMoveToPoint(context, CGRectGetMinX(bounds), CGRectGetMidY(bounds));
+        //
+        //    period one
+        //
+    CGContextAddQuadCurveToPoint(context, 1.0 * CGRectGetWidth(bounds) / 4.0, -0.4375 * CGRectGetMaxY(bounds),
+                                 2.0 * CGRectGetWidth(bounds) / 4.0, CGRectGetMidY(bounds));
+    
+    CGContextAddQuadCurveToPoint(context, 3.0 * CGRectGetWidth(bounds) / 4.0, 1.4375 * CGRectGetMaxY(bounds),
+                                 4.0 * CGRectGetWidth(bounds) / 4.0, CGRectGetMidY(bounds));
+        //
+        //    period two
+        //
+    CGContextAddQuadCurveToPoint(context, 5.0 * CGRectGetWidth(bounds) / 4.0, -0.0625 * CGRectGetMaxY(bounds),
+                                 6.0 * CGRectGetWidth(bounds) / 4.0, CGRectGetMidY(bounds));
+    
+    CGContextAddQuadCurveToPoint(context, 7.0 * CGRectGetWidth(bounds) / 4.0, 1.0625 * CGRectGetMaxY(bounds),
+                                 8.0 * CGRectGetWidth(bounds) / 4.0, CGRectGetMidY(bounds));
+        //
+        //    period three
+        //
+    CGContextAddQuadCurveToPoint(context, 9.0 * CGRectGetWidth(bounds) / 4.0, -0.4375 * CGRectGetMaxY(bounds),
+                                 10.0 * CGRectGetWidth(bounds) / 4.0, CGRectGetMidY(bounds));
+    
+    CGContextAddQuadCurveToPoint(context, 11.0 * CGRectGetWidth(bounds) / 4.0, 1.4375 * CGRectGetMaxY(bounds),
+                                 12.0 * CGRectGetWidth(bounds) / 4.0, CGRectGetMidY(bounds));
+        //
+        //    period four
+        //
+    CGContextAddQuadCurveToPoint(context, 13.0 * CGRectGetWidth(bounds) / 4.0, -0.125 * CGRectGetMaxY(bounds),
+                                 14.0 * CGRectGetWidth(bounds) / 4.0, CGRectGetMidY(bounds));
+    
+    CGContextAddQuadCurveToPoint(context, 15.0 * CGRectGetWidth(bounds) / 4.0, 1.125 * CGRectGetMaxY(bounds),
+                                 16.0 * CGRectGetWidth(bounds) / 4.0, CGRectGetMidY(bounds));
+    
+    CGContextRestoreGState(context);
+    CGContextSetStrokeColorWithColor(context, [[UIColor greenColor] CGColor]);
+    
+    CGContextStrokePath(context);
+
 }
 
 - (void)drawRect:(CGRect)rect
@@ -118,10 +177,7 @@ static  CGFloat  kDisabledPowerLevel = -1.0;
         CGContextSetFillColorWithColor(context, self.disabledStrokeColor.CGColor);
         CGContextStrokePath(context);
     }
-    
-    CGContextAddEllipseInRect(context, self.innerDiskRectangle);
-    CGContextSetFillColorWithColor(context, self.innerDiskColor.CGColor);
-    CGContextFillPath(context);
+    [self drawInnerDiskInContext:context withBounds:bounds];
 }
 
 @end
