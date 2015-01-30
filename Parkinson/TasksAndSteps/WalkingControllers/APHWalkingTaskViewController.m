0b64eacf0959551af692e7ca231fb735c66a1cf3
Henry McGilton <henry.mcgilton@ymedialabs.com>
2015-01-29 16:21:27 -0800

    APPLE-1291 — Transition To Research Kit Version 2.0
    Added code to collect number of steps while Walking Activity is in progress.
    
    Added code to notify user that Walking Activity has completed.

--- Parkinson/TasksAndSteps/WalkingControllers/APHWalkingTaskViewController.m
+++ Parkinson/TasksAndSteps/WalkingControllers/APHWalkingTaskViewController.m
@@ -6,18 +6,35 @@
 // 
  
 #import "APHWalkingTaskViewController.h"
-//#import <HealthKit/HealthKit.h>
-//#import <AVFoundation/AVFoundation.h>
-//#import <AudioToolbox/AudioToolbox.h>
-//#import "APHActiveStepViewController.h"
+#import <HealthKit/HealthKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-#import <objc/message.h>
+typedef  enum  _WalkingStepOrdinals
+{
+    WalkingStepOrdinalsInstructionStep = 0,
+    WalkingStepOrdinalsCountdownStep,
+    WalkingStepOrdinalsWalkOutStep,
+    WalkingStepOrdinalsWalkBackStep,
+    WalkingStepOrdinalsStandStillStep,
+    WalkingStepOrdinalsConclusionStep,
+}  WalkingStepOrdinals;
 
-static  NSString       *kWalkingActivityTitle = @"Timed Walking Task";
+static  NSString       *kWalkingActivityTitle = @"Walking Activity";
 
 static  NSUInteger      kNumberOfStepsPerLeg  = 20;
 static  NSTimeInterval  kStandStillDuration   = 30.0;
 
+
+@interface APHWalkingTaskViewController  ( )
+
+@property  (nonatomic, assign)  WalkingStepOrdinals  walkingStepOrdinal;
+
+@property  (nonatomic, strong)  NSDate              *startCollectionDate;
+@property  (nonatomic, strong)  NSDate              *endCollectionDate;
+
+@property  (nonatomic, assign)  NSInteger  __block   collectedNumberOfSteps;
+
+@end
 
 @implementation APHWalkingTaskViewController
 
@@ -29,16 +46,63 @@ static  NSTimeInterval  kStandStillDuration   = 30.0;
                             intendedUseDescription:nil
                             numberOfStepsPerLeg:kNumberOfStepsPerLeg
                             restDuration:kStandStillDuration
-                            options:0];
+                            options:RKPredefinedTaskOptionNone];
     return  task;
 }
 
-
-#pragma  mark  -  Results For Dashboard
+#pragma  mark  -  Create Dashboard Summary Results
 
 - (NSString *)createResultSummary
 {
-    return @"";
+    NSDictionary  *summary = @{  @"value" : @(self.collectedNumberOfSteps) };
+    
+    NSError  *error = nil;
+    NSData  *data = [NSJSONSerialization dataWithJSONObject:summary options:0 error:&error];
+    NSString  *contentString = nil;
+    if (data == nil) {
+        APCLogError2 (error);
+    } else {
+        contentString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    }
+    return  contentString;
+}
+
+#pragma  mark  -  Task View Controller Delegate Methods
+
+- (void)taskViewController:(RKSTTaskViewController *)taskViewController stepViewControllerWillAppear:(RKSTStepViewController *)stepViewController
+{
+    if (self.walkingStepOrdinal == WalkingStepOrdinalsWalkOutStep) {
+        self.startCollectionDate = [NSDate date];
+    }
+    if (self.walkingStepOrdinal == WalkingStepOrdinalsStandStillStep) {
+        self.endCollectionDate = [NSDate date];
+
+        HKQuantityType  *stepCountType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierStepCount];
+        NSPredicate  *predicate = [HKQuery predicateForSamplesWithStartDate:self.startCollectionDate endDate:self.endCollectionDate options:HKQueryOptionNone];
+
+        HKStatisticsQuery  *query = [[HKStatisticsQuery alloc] initWithQuantityType:stepCountType
+                                                            quantitySamplePredicate:predicate
+                                                                            options:HKStatisticsOptionCumulativeSum
+                                                                  completionHandler:^(HKStatisticsQuery *query, HKStatistics *result, NSError *error) {
+                                                                      if (result != nil) {
+                                                                          self.collectedNumberOfSteps = [result.sumQuantity doubleValueForUnit:[HKUnit countUnit]];
+                                                                          NSLog(@"APHWalkingTaskViewController completionHandler self.collectedNumberOfSteps %lu", (unsigned long)(self.collectedNumberOfSteps));
+                                                                      } else {
+                                                                          APCLogError2 (error);
+                                                                      }
+                                                                  }];
+        HKHealthStore  *healthStore = [HKHealthStore new];
+        [healthStore executeQuery:query];
+    }
+    if (self.walkingStepOrdinal == WalkingStepOrdinalsConclusionStep) {
+        AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+        AVSpeechUtterance  *talk = [AVSpeechUtterance
+                                    speechUtteranceWithString:NSLocalizedString(@"You have completed the activity.", @"You have completed the activity.")];
+        AVSpeechSynthesizer  *synthesiser = [[AVSpeechSynthesizer alloc] init];
+        talk.rate = 0.1;
+        [synthesiser speakUtterance:talk];
+    }
+    self.walkingStepOrdinal = self.walkingStepOrdinal + 1;
 }
 
 #pragma  mark  -  View Controller Methods
@@ -46,6 +110,8 @@ static  NSTimeInterval  kStandStillDuration   = 30.0;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.walkingStepOrdinal = WalkingStepOrdinalsInstructionStep;
     
     self.navigationBar.topItem.title = NSLocalizedString(kWalkingActivityTitle, nil);
 }
