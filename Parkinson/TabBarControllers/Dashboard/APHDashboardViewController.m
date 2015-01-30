89b728461384a6e342239191d55a73593ed14815
Henry McGilton <henry.mcgilton@ymedialabs.com>
2015-01-29 17:45:01 -0800

    Re-phrase Dashboard Title from  'Past 5 Days'  to  'Recent Activity'

--- Parkinson/TabBarControllers/Dashboard/APHDashboardViewController.m
+++ Parkinson/TabBarControllers/Dashboard/APHDashboardViewController.m
@@ -218,7 +218,7 @@ static NSString * const kAPCRightDetailTableViewCellIdentifier = @"APCRightDetai
         
         APCTableViewSection *section = [APCTableViewSection new];
         section.rows = [NSArray arrayWithArray:rowItems];
-        section.sectionTitle = NSLocalizedString(@"Past 5 Days", @"");
+        section.sectionTitle = NSLocalizedString(@"Recent Activity", @"");
         [self.items addObject:section];
     }
     
