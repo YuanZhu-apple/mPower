b15cb283724635f7c0a24f36dc25bea8926e4299
Henry McGilton <henry.mcgilton@ymedialabs.com>
2015-02-20 11:37:15 -0800

    [APPLE-1967] Re-instate Medication Tracking Task View Controller.
    For the fourth or fifth time in a row . . .
    
    This commit relies on the previous commit of the JSON Task and Schedules file.
    
    The previous commit will create an App that will build but may crash if the user
    taps the Medication Tracking table row.

---
+++ Parkinson/TasksAndSteps/MedicationTracking/APHMedicationTrackerTaskViewController.h
@@ -0,0 +1,15 @@
+// 
+//  APHMedicationTrackerTaskViewController.h
+//  mPower 
+// 
+//  Copyright (c) 2014 Apple, Inc. All rights reserved.
+// 
+ 
+#import <Foundation/Foundation.h>
+
+#import <ResearchKit/ResearchKit.h>
+#import <APCAppCore/APCAppCore.h>
+
+@interface APHMedicationTrackerTaskViewController : APCBaseTaskViewController
+
+@end
