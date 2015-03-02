4920f90cd407b04f64931622dcc93015899c385d
Pablo Arce <pablo.arce@ymedialabs.com>
2015-03-01 21:32:42 -0800

    Updated content

---
+++ Parkinson/TasksAndSteps/APHenrollmentsurveyViewController.h
@@ -0,0 +1,13 @@
+//
+//  APHenrollmentsurveyViewController.h
+//  Parkinson
+//
+//  Created by Pablo Arce on 3/1/15.
+//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//
+
+#import <APCAppCore/APCAppCore.h>
+
+@interface APHenrollmentsurveyViewController : APCGenericSurveyTaskViewController
+
+@end
