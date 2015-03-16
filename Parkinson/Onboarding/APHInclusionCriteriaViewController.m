42da39006131cc7352d1d3feaacd453fde894c3e
David Barnard <david.barnard@ymedialabs.com>
2015-03-16 17:05:55 -0400

    [APPLE-2128] Used new AppCore defined UIFont category methods to replace common font usages.

--- Parkinson/Onboarding/APHInclusionCriteriaViewController.m
+++ Parkinson/Onboarding/APHInclusionCriteriaViewController.m
@@ -52,26 +52,26 @@
 {
     {
         self.question1Label.textColor = [UIColor appSecondaryColor1];
-        self.question1Label.font = [UIFont appRegularFontWithSize:17.0f];
+        self.question1Label.font = [UIFont appQuestionLabelFont];
         
-        [self.question1Option1.titleLabel setFont:[UIFont appRegularFontWithSize:44.0]];
-        [self.question1Option2.titleLabel setFont:[UIFont appRegularFontWithSize:44.0]];
+        [self.question1Option1.titleLabel setFont:[UIFont appQuestionOptionFont]];
+        [self.question1Option2.titleLabel setFont:[UIFont appQuestionOptionFont]];
     }
     
     {
         self.question2Label.textColor = [UIColor appSecondaryColor1];
-        self.question2Label.font = [UIFont appRegularFontWithSize:17.0f];
+        self.question2Label.font = [UIFont appQuestionLabelFont];
         
-        [self.question2Option1.titleLabel setFont:[UIFont appRegularFontWithSize:44.0]];
-        [self.question2Option2.titleLabel setFont:[UIFont appRegularFontWithSize:44.0]];
+        [self.question2Option1.titleLabel setFont:[UIFont appQuestionOptionFont]];
+        [self.question2Option2.titleLabel setFont:[UIFont appQuestionOptionFont]];
     }
     
     {
         self.question3Label.textColor = [UIColor appSecondaryColor1];
-        self.question3Label.font = [UIFont appRegularFontWithSize:17.0f];
+        self.question3Label.font = [UIFont appQuestionLabelFont];
         
-        [self.question3Option1.titleLabel setFont:[UIFont appRegularFontWithSize:44.0]];
-        [self.question3Option2.titleLabel setFont:[UIFont appRegularFontWithSize:44.0]];
+        [self.question3Option1.titleLabel setFont:[UIFont appQuestionOptionFont]];
+        [self.question3Option2.titleLabel setFont:[UIFont appQuestionOptionFont]];
     }
     
 }
