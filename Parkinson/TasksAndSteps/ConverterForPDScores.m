dc48be7d5f3fce2e47f898c0cd9921eea833e6d4
Dzianis Asanovich <dzianis.asanovich@ymedialabs.com>
2015-03-20 12:09:39 -0700

    Remove name from file description

--- Parkinson/TasksAndSteps/ConverterForPDScores.m
+++ Parkinson/TasksAndSteps/ConverterForPDScores.m
@@ -1,9 +1,8 @@
 //
 //  ConverterForPDScores.m
-//  Parkinson
+//  mPower
 //
-//  Created by Dzianis Asanovich on 2/20/15.
-//  Copyright (c) 2015 Apple, Inc. All rights reserved.
+//  Copyright (c) 2014 Apple, Inc. All rights reserved.
 //
 
 #import "ConverterForPDScores.h"
